fa0015c8369ff6762e4be4be9af4e3e086f5d033
revforyou <103939888+revforyou@users.noreply.github.com>
2024-05-05 17:15:01 -0400

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -1,1 +1,11 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [math.inf]*(amount+1)
+        dp[0] = 0
 
+        for a in range(1,amount+1):
+            for c in coins:
+                if c<=a:
+                    dp[a] = min(dp[a],1+dp[a-c])
+
+        return dp[-1] if dp[-1]!=math.inf else -1
